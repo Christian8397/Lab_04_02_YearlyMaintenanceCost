f24dc2fd0be0a672c17e9446c256e3dfca9d1a39
Christian8397 <bachci@mail.uc.edu>
2025-09-20 15:53:18 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+     double springCost = 100;
+     double summerCost = 200;
+     double fallCost = 150;
+     double winterCost = 300;
+
+     double totalYearlyCost = summerCost + springCost + fallCost + winterCost;
+        System.out.println("Total Yearly Cost: " + totalYearlyCost);
+    }
+}
